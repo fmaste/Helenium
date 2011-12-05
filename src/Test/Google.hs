a46255f5f0b866c06a5d5b9b6cec99d5d8794974
fmaste <fmaste@users.noreply.github.com>
2011-12-05 16:20:43 -0300

    Use as template

--- src/Test/Google.hs
+++ src/Test/Google.hs
@@ -6,15 +6,17 @@ main = runTest test
 
 -- Test to search google on Google!
 test = do
-	goTo "http://www.google.com"
+	goTo "http://www.google.com/ncr"
 	searchInput <- getElementById "lst-ib"
 	sendKeysToElement searchInput "google"
-	--sleep 5
-	--submitElement searchInput
-	--back
-	--forward
+	sleep 5
+	goTo "http://www.amazon.com"
+	back
+	forward
 	refresh
-	--url <- getUrl
-	--title <- getTitle
+	url <- getUrl
+	assertEqual "http://www.amazon.com/" url
+	title <- getTitle
+	assertPrefix "Amazon.com" title
 	return ()
 
