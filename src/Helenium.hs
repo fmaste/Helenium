1ae0f09eebfcaca3cdd5b7e7a6a5c63a193ebe0d
fmaste <fmaste@users.noreply.github.com>
2011-12-06 13:08:58 -0300

    Create base package

--- src/Helenium.hs
+++ src/Helenium.hs
@@ -1,10 +1,4 @@
 module Helenium (
-	HeleniumM,
-	HeleniumReader (..),
-	HeleniumBrowser (..),
-	HeleniumBrowserName (..),
-	HeleniumBrowserVersion,
-	HeleniumBrowserPlatform (..),
 	runTest,
 	echo,
 	getCurrentTime,
@@ -53,6 +47,7 @@ module Helenium (
 
 -------------------------------------------------------------------------------
 
+import Helenium.Base
 import Data.Maybe
 import Data.Either
 import qualified Data.Time as Time
@@ -65,90 +60,6 @@ import qualified Network.HTTP.Stream as Stream
 import qualified Text.JSON as JSON
 import System (getArgs)
 import qualified System.Posix.Unistd as Sys
-
--------------------------------------------------------------------------------
-
--- Helenium Monad
-
-type HeleniumM =
-	ErrorT HeleniumError (RWST HeleniumReader HeleniumWriter HeleniumState IO)
-
-runHeleniumM ::
-	HeleniumM a ->
-	HeleniumReader ->
-	HeleniumState ->
-	IO (Either HeleniumError a, HeleniumState, HeleniumWriter)
-runHeleniumM hm r s = runRWST (runErrorT hm) r s
-
-{- 
-Hint:
-:t runHeleniumT
-    runHeleniumT :: HeleniumT e r w s m a -> ErrorT e (RWST r w s m) a
-:t runErrorT . runHeleniumT
-    runErrorT . runHeleniumT :: HeleniumT e r w s m a -> RWST r w s m (Either e a)
-:t runRWST . runErrorT . runHeleniumT
-    runRWST . runErrorT . runHeleniumT :: HeleniumT e r w s m a -> r -> s -> m (Either e a, s, w)
--}
-
-type HeleniumError = String
-
-data HeleniumReader = 
-	HeleniumReader {
-		name :: String,
-		server :: String,
-		browser :: HeleniumBrowser,
-		logTime :: Bool,
-		debugHttp :: Bool,
-		debugTime :: Bool,
-		timeoutTest :: Int,
-		timeoutElement :: Int,
-		screenshotPath :: String
-	}
-
-type HeleniumWriter = [(Time.UTCTime, HeleniumWriterLevel, HeleniumWriterMsg)]
-
--- TODO: This should be only INFO, DEBUG, WARN and ERROR
-data HeleniumWriterLevel = Info | Debug | DebugRequest | DebugResponse
-
--- TODO: Implement different type of messages.
--- Simples with a String, with a Request object, with a screenshot, etc.
-type HeleniumWriterMsg = String
-
-data HeleniumState = 
-	HeleniumState {
-		serverCapabilities :: [HeleniumCapability],
-		serverSessionId :: Maybe HeleniumSessionId
-	}
-
-type HeleniumSessionId = String
-
-data HeleniumBrowser = 
-	HeleniumBrowser {
-		browserName :: HeleniumBrowserName,
-		browserVersion :: HeleniumBrowserVersion,
-		browserPlatform :: HeleniumBrowserPlatform
-	}
-
-data HeleniumBrowserName = Chrome | Firefox | HtmlUnit | IE | IPhone
-
-type HeleniumBrowserVersion = String
-
-data HeleniumBrowserPlatform = Windows| XP | Vista | Mac | Linux | Unix | Any
-
--- TODO: Move to the reader after moving out the config from the tests.
-data HeleniumCapability =
-	JavascriptEnabled |
-	TakesScreenshot |
-	HandlesAlerts |
-	DatabaseEnabled |
-	LocationContextEnabled |
-	ApplicationCacheEnabled |
-	BrowserConnectionEnabled |
-	CssSelectorsEnabled |
-	WebStorageEnabled |
-	Rotatable |
-	AcceptSslCerts |
-	NativeEvents
 
 -- Test runner.
 -------------------------------------------------------------------------------
---
+++ src/Helenium/Base.hs
@@ -0,0 +1,107 @@
+module Helenium.Base (
+	HeleniumM,
+	runHeleniumM,
+	HeleniumError,
+	HeleniumReader (..),
+	HeleniumWriter (..),
+	HeleniumState (..),
+	HeleniumBrowser (..),
+	HeleniumBrowserName (..),
+	HeleniumBrowserVersion,
+	HeleniumBrowserPlatform (..),
+	HeleniumWriterLevel (..),
+	HeleniumWriterMsg,
+	HeleniumCapability (..)
+) where
+
+-------------------------------------------------------------------------------
+
+import Data.Maybe
+import qualified Data.Time as Time
+import Control.Monad.Error
+import Control.Monad.RWS.Strict
+
+-------------------------------------------------------------------------------
+
+-- Helenium Monad
+
+type HeleniumM =
+	ErrorT HeleniumError (RWST HeleniumReader HeleniumWriter HeleniumState IO)
+
+runHeleniumM ::
+	HeleniumM a ->
+	HeleniumReader ->
+	HeleniumState ->
+	IO (Either HeleniumError a, HeleniumState, HeleniumWriter)
+runHeleniumM hm r s = runRWST (runErrorT hm) r s
+
+{- 
+Hint:
+:t runHeleniumT
+  runHeleniumT :: HeleniumT e r w s m a -> ErrorT e (RWST r w s m) a
+:t runErrorT . runHeleniumT
+  runErrorT . runHeleniumT :: HeleniumT e r w s m a -> RWST r w s m (Either e a)
+:t runRWST . runErrorT . runHeleniumT
+  runRWST . runErrorT . runHeleniumT :: HeleniumT e r w s m a -> r -> s -> m (Either e a, s, w)
+-}
+
+type HeleniumError = String
+
+data HeleniumReader =
+	HeleniumReader {
+		name :: String,
+		server :: String,
+		browser :: HeleniumBrowser,
+		logTime :: Bool,
+		debugHttp :: Bool,
+		debugTime :: Bool,
+		timeoutTest :: Int,
+		timeoutElement :: Int,
+		screenshotPath :: String
+	}
+
+type HeleniumWriter = [(Time.UTCTime, HeleniumWriterLevel, HeleniumWriterMsg)]
+
+-- TODO: This should be only INFO, DEBUG, WARN and ERROR
+data HeleniumWriterLevel = Info | Debug | DebugRequest | DebugResponse
+
+-- TODO: Implement different type of messages.
+-- Simples with a String, with a Request object, with a screenshot, etc.
+type HeleniumWriterMsg = String
+
+data HeleniumState =
+	HeleniumState {
+		serverCapabilities :: [HeleniumCapability],
+		serverSessionId :: Maybe HeleniumSessionId
+	}
+
+type HeleniumSessionId = String
+
+data HeleniumBrowser =
+	HeleniumBrowser {
+		browserName :: HeleniumBrowserName,
+		browserVersion :: HeleniumBrowserVersion,
+		browserPlatform :: HeleniumBrowserPlatform
+	}
+
+data HeleniumBrowserName = Chrome | Firefox | HtmlUnit | IE | IPhone
+
+type HeleniumBrowserVersion = String
+
+data HeleniumBrowserPlatform = Windows| XP | Vista | Mac | Linux | Unix | Any
+
+--- TODO: Move to the reader after moving out the config from the tests.
+data HeleniumCapability =
+	JavascriptEnabled |
+	TakesScreenshot |
+	HandlesAlerts |
+	DatabaseEnabled |
+	LocationContextEnabled |
+	ApplicationCacheEnabled |
+	BrowserConnectionEnabled |
+	CssSelectorsEnabled |
+	WebStorageEnabled |
+	Rotatable |
+	AcceptSslCerts |
+	NativeEvents
+
