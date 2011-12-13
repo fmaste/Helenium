240dfd73beb0a887a52d4c24eae7170f12561a09
fmaste <fmaste@users.noreply.github.com>
2011-12-13 13:39:46 -0300

    Extract the HTML part

--- src/Helenium/Log.hs
+++ src/Helenium/Log.hs
@@ -22,19 +22,24 @@ logMsg msg = do
 
 logGenerator :: H.HeleniumWriter -> String
 logGenerator logs = htmlPrefix ++ (concat $ map f logs) ++ htmlSuffix where
-	f (t, m) = "<tr>" ++ (showLogTime t) ++ (showLogMsg m) ++ "</tr>"
+	f (t, m) = "<tr><td>" ++ (showLogTime t) ++ "</td><td>" ++ (showLogType m) ++ "</td><td>" ++ (showLogMsg m) ++ "</td></tr>"
 
 htmlPrefix = "<html><head><style>td{border: 1px solid #000;}</style></head><body><table>"
 htmlSuffix = "</table></body></html>"
 
 showLogTime :: Time.UTCTime -> String
 -- TODO: Sometimes the miliseconds has 3 numbers and sometimes 4.
-showLogTime t = "<td>" ++ (show t) ++ "</td>"
+showLogTime t = show t
+
+showLogType :: H.HeleniumWriterMsg -> String
+showLogType (H.Info msg) = "INFO"
+showLogType (H.HttpRequest req) = "REQUEST"
+showLogType (H.HttpResponse res) = "RESPONSE"
+showLogType (H.Screenshot s) = "SCREENSHOT"
 
 showLogMsg :: H.HeleniumWriterMsg -> String
-showLogMsg (H.Info msg) = "<td>INFO</td><td>" ++ msg ++ "</td>"
-showLogMsg (H.HttpRequest req) = "<td>REQUEST</td><td>" ++ req ++ "</td>"
-showLogMsg (H.HttpResponse res) = "<td>RESPONSE</td><td>" ++ res ++ "</td>"
-showLogMsg (H.Screenshot s) = "<td>SCREENSHOT</td><td><img src=\"data:image/png;base64," ++ s ++ "\" /></td>"
-
+showLogMsg (H.Info msg) = msg
+showLogMsg (H.HttpRequest req) = req
+showLogMsg (H.HttpResponse res) = res
+showLogMsg (H.Screenshot s) = "<img src=\"data:image/png;base64," ++ s ++ "\" />"
 
