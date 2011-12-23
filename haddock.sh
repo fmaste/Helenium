40cfdfde49c70d2c37c7cae0e6b69dfc8ecbb149
fmaste <fmaste@users.noreply.github.com>
2011-12-23 12:23:14 -0300

    Forgot commands package

--- haddock.sh
+++ haddock.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-haddock --odir=doc --html --use-unicode --title=Helenium src/Helenium.hs src/Helenium/Base.hs src/Helenium/Runner.hs src/Helenium/Network.hs src/Helenium/Log.hs
+haddock --odir=doc --html --use-unicode --title=Helenium src/Helenium.hs src/Helenium/Base.hs src/Helenium/Commands.hs src/Helenium/Runner.hs src/Helenium/Network.hs src/Helenium/Log.hs
 
 ## src/Helenium/Base.hs src/Helenium/Runner.hs src/Helenium/Network.hs src/Helenium/Log.hs
 
